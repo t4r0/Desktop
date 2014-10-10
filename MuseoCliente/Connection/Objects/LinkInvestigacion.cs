c91a54dfd91a0fcd754f8bfa1ba3bff5cce85086
Bryan Anleu <bryanangom@hotmail.com>
2014-10-10 09:23:12 -0600

    clases
    base de datos trasladada a clases

---
+++ MuseoCliente/Connection/Objects/LinkInvestigacion.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections;
+
+namespace MuseoCliente.Connection.Objects
+{
+    public class LinkInvestigacion:ResourceObject<LinkInvestigacion>
+    {
+        public LinkInvestigacion():base("v1/categoria/")
+        {
+
+        }
+        
+        [JsonProperty]
+        public string link { get; set; }
+    }
+}
