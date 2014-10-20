b6e19a29821042ec20321cb55efb27e25a31b32a
Bryan Anleu <bryanangom@hotmail.com>
2014-10-20 10:32:00 -0600

    Funciones1
    Funciones version 1

--- MuseoCliente/Connection/Objects/Pais.cs
+++ MuseoCliente/Connection/Objects/Pais.cs
@@ -29,5 +29,56 @@ namespace MuseoCliente.Connection.Objects
 
         [JsonProperty]
         public int num_code { get; set; }
+
+
+        public void guardar() //Crear un Usuario
+        {
+            try
+            {
+                this.Create();
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(3, "No se ha guardado en la Informacion en la base de datos");
+            }
+        }
+
+
+        public void modificar(int id) //Modifica datos de un pais
+        {
+            try
+            {
+                this.Save(id.ToString());
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(4, "No se ha modifico en la Informacion en la base de datos");
+            }
+        }
+
+
+        public ArrayList consultarPaises() //Devuelve lista de todos los paises ingresados
+        {
+            ArrayList listaNueva = new ArrayList();
+            try
+            {
+
+                ICollection<Pais> todosPaises = (ICollection<Pais>)this.GetAsCollection();
+                var paisNombre = from country in todosPaises
+                                  select name;
+                listaNueva.AddRange((ICollection)paisNombre);
+
+                if (listaNueva == null)
+                    Error.ingresarError(2, "No hay paises existentes");
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(2, "No hay paises existentes");
+            }
+
+            return new ArrayList(listaNueva);
+        }
+
+
     }
 }
