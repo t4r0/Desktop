a36e7f963ab3e501eb59564743ea746eb191a26d
Bryan Anleu <bryanangom@hotmail.com>
2014-10-19 22:41:49 -0600

    Clases
    Usuario, publicacion, pais

---
+++ MuseoCliente/Connection/Objects/Pais.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections;
+
+namespace MuseoCliente.Connection.Objects
+{
+    public class Pais : ResourceObject<Pais>
+    {
+ 
+        public Pais()
+            : base("/v1/paises/")
+        {
+
+        }
+
+        [JsonProperty]
+        public string name { get; set; }
+
+        [JsonProperty]
+        public string printable_name { get; set; }
+
+        [JsonProperty]
+        public string iso3 { get; set; }
+
+        [JsonProperty]
+        public int num_code { get; set; }
+    }
+}
---
+++ MuseoCliente/Connection/Objects/Publicacion.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Newtonsoft.Json;
+using System.Net.Http;
+using System.Collections;
+namespace MuseoCliente.Connection.Objects
+{
+    public class Publicacion : ResourceObject<Publicacion>
+    {
+
+        public Publicacion()
+            : base("v1/publicacion/")
+        {
+            this.estructura = new Estructura();
+
+        }
+
+        [JsonProperty]
+        public DateTime fecha { get; set; }
+
+        [JsonProperty]
+        public string nombre { get; set; }
+
+        [JsonProperty]
+        public string publicacion { get; set; }
+
+        [JsonProperty]
+        public string link { get; set; }
+
+        public Ficha()
+            : base("v1/fichas/")
+        {
+            this.estructura = new Estructura();
+
+        }
+    }
+}
