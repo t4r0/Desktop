28b17d815c3a5903dcad9d4b4f1e4f9e9a5fe49b
Victor Betancohurt <victorbetha@gmail.com>
2014-10-20 00:37:57 -0600

    MODIFIACION
    se crearon Autor ficha fotografia mantenimeitno registro Valor Check

--- MuseoCliente/Connection/Objects/Mantenimiento.cs
+++ MuseoCliente/Connection/Objects/Mantenimiento.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Newtonsoft.Json;
+using System.Collections;
 
 namespace MuseoCliente.Connection.Objects
 {
@@ -19,8 +20,55 @@ namespace MuseoCliente.Connection.Objects
         public int consolidacion { get; set; }
 
         public Mantenimiento()
-            : base("kas/asjd/las")
+            : base("/v1/mantenimiento/")
         {
+        }
+
+        public void guardar()
+        {
+            try
+            {
+                this.Create();
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(3, "No se ha guardado en la Informacion en la base de datos");
+            }
+        }
+
+        public void modificar(string id)
+        {
+            try
+            {
+                this.Save(id);
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(4, "No se ha modifico en la Informacion en la base de datos");
+            }
+        }
+
+        public ArrayList consultarNombre(int consolidacion)
+        {
+            ArrayList listaNueva = new ArrayList();
+            try
+            {
+
+                ICollection<Mantenimiento> todasMantenimiento = (ICollection<Mantenimiento>)this.GetAsCollection();
+                var mantenimientoConsolidacion = from mantenimeinto in todasMantenimiento
+                                                 where mantenimeinto.consolidacion == consolidacion
+                                                 select mantenimeinto;
+                listaNueva.AddRange((ICollection)mantenimientoConsolidacion);
+
+                if (listaNueva == null)
+                    Error.ingresarError(2, "No se encontro nombre similares");
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(2, "No se encontro nombre similares");
+            }
+
+            return new ArrayList(listaNueva);
         }
     }
 }
--- MuseoCliente/Connection/Objects/ValorCheck.cs
+++ MuseoCliente/Connection/Objects/ValorCheck.cs
@@ -11,15 +11,88 @@ namespace MuseoCliente.Connection.Objects
 {
     public class ValorCheck : ResourceObject<ValorCheck>
     {
-        public ValorCheck():base("v1/fichas/")
-        {
-
-        }
+        
 
         [JsonProperty]
         public string nombre { get; set; }
 
         [JsonProperty]
         public Boolean seleccionado { get; set; }
+        
+        public ValorCheck():base("v1/fichas/")
+        {
+
+        }
+
+        public void guardar()
+        {
+            try
+            {
+                this.Create();
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(3, "No se ha guardado en la Informacion en la base de datos");
+            }
+        }
+
+        public void modificar(string id)
+        {
+            try
+            {
+                this.Save(id);
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(4, "No se ha modifico en la Informacion en la base de datos");
+            }
+        }
+
+        public ArrayList consultarNombre(String nombre)
+        {
+            ArrayList listaNueva = new ArrayList();
+            try
+            {
+
+                ICollection<ValorCheck> todasValor = (ICollection<ValorCheck>)this.GetAsCollection();
+                var valorNombres = from valor in todasValor
+                                   where valor.nombre.Contains(nombre)
+                                   select valor;
+                listaNueva.AddRange((ICollection)valorNombres);
+
+                if (listaNueva == null)
+                    Error.ingresarError(2, "No se encontro nombre similares");
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(2, "No se encontro nombre similares");
+            }
+
+            return new ArrayList(listaNueva);
+        }
+
+        public ArrayList consultarSeleccionado(Boolean seleccionado)
+        {
+            ArrayList listaNueva = new ArrayList();
+            try
+            {
+
+                ICollection<ValorCheck> todasValor = (ICollection<ValorCheck>)this.GetAsCollection();
+                var valorSeleccionado = from valor in todasValor
+                                   where valor.seleccionado ==  seleccionado
+                                   select valor;
+                listaNueva.AddRange((ICollection)valorSeleccionado);
+
+                if (listaNueva == null)
+                    Error.ingresarError(2, "No se encontro nombre similares");
+            }
+            catch (Exception e)
+            {
+                Error.ingresarError(2, "No se encontro nombre similares");
+            }
+
+            return new ArrayList(listaNueva);
+        }
+
     }
 }
