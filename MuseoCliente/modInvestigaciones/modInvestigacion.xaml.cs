bf5d95f9883a29a7988fdda5821a6aee54971785
JEscalante <JEscalanteGT@gmail.com>
2014-11-07 12:17:04 -0600

    Investigación - agregar piezas

--- MuseoCliente/modInvestigaciones/modInvestigacion.xaml.cs
+++ MuseoCliente/modInvestigaciones/modInvestigacion.xaml.cs
@@ -10,6 +10,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using MuseoCliente.Connection.Objects;
+using System.Threading.Tasks;
+using System.Collections;
 
 namespace MuseoCliente
 {
@@ -19,9 +22,9 @@ namespace MuseoCliente
 	public partial class modInvestigacion : UserControl
 	{
         //Pendiente Editor: Porque es el usuario que ingresa -
-        Connection.Objects.Pieza piezas = new Connection.Objects.Pieza();
-        Connection.Objects.Autor autores = new Connection.Objects.Autor();
-        Connection.Objects.Investigacion investigacion = new Connection.Objects.Investigacion();
+        Pieza piezas = new Pieza();
+        Autor autores = new Autor();
+        Investigacion investigacion = new Investigacion();
         public UserControl anterior;
         public Border borde;
         public bool modificar = false;
@@ -50,20 +53,22 @@ namespace MuseoCliente
             {
                 lblOperacion.Content = "Nueva Categoría";
                 cmbAutor.ItemsSource = autores.regresarTodos();
+                gvPiezasGuardadas.ItemsSource = new ArrayList();
             }
         }
 
         private void btnGuardar_Click(object sender, RoutedEventArgs e)
         {
-            investigacion.titulo = txtTitulo.Text;
-            investigacion.contenido = txtDescripcion.Text;
-            investigacion.resumen = txtResumen.Text;
+            investigacion = (Investigacion) this.DataContext;
             // Error en la clase = investigacion.autor = Convert.ToInt16(cmbAutor.SelectedValue.ToString());
-            investigacion.publicado = publicado; //Verificar la clase porque en la BD esta como tinyInt
             // Pregunta: la fecha es con un Now()? o puede seleccionar fecha? investigacion.fecha = ????;
+            investigacion.autor = (int) cmbAutor.SelectedValue;
+            investigacion.editor = "JEscalante";
+            List<string> listado = LinksReferencia.GetOptions();
+           
             if (modificar == false)
             {
-                investigacion.guardar();
+                //investigacion.guardar();
             }
             else
             {
@@ -103,6 +108,78 @@ namespace MuseoCliente
         private void btnCancelar_Click(object sender, RoutedEventArgs e)
         {
             borde.Child = anterior;
+        }
+
+        private void btnSeleccionar_Click(object sender, RoutedEventArgs e)
+        {
+            string codigo = txtCodigo.Text;
+            buscarPiezas(codigo);
+        }
+        private async void buscarPiezas(string codigo)
+        {
+            Task<ArrayList> task = Task<ArrayList>.Factory.StartNew(()=>piezas.buscarNombre(codigo));
+            await task;
+            gvPiezas.ItemsSource = task.Result;
+        }
+
+        private void txtCodigo_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtCodigo.Text.Length > 3)
+            {
+                buscarPiezas(txtCodigo.Text);
+            }
+            else
+            {
+                gvPiezas.ItemsSource = null;
+            }
+        }
+
+        private void Button_Click_1(object sender, RoutedEventArgs e)
+        {
+            Pieza piezaSeleccionada = (Pieza) gvPiezas.SelectedItem;
+
+            gvPiezasGuardadas.ItemsSource = this.verificarPieza(piezaSeleccionada);
+            gvPiezasGuardadas.Items.Refresh();
+            //gvPiezasGuardadas.Items.Add(piezaSeleccionada);
+        }
+        private ArrayList verificarPieza(Pieza pieza)
+        {
+            ArrayList listado = (ArrayList) gvPiezasGuardadas.ItemsSource;
+            Boolean existePieza = false;
+            if (listado.Count == 0) 
+            {
+                listado.Add(pieza);
+                existePieza = true;
+            } 
+            for (int i = 0; i < listado.Count; i++) 
+            {
+                Pieza piezaActual = (Pieza) listado[i];
+
+                if (piezaActual.codigo == pieza.codigo)
+                {
+                    existePieza = true;
+                    break;
+                }
+            }
+            if (!existePieza)
+                listado.Add(pieza);
+            return listado;
+        }
+        private void Button_Click_2(object sender, RoutedEventArgs e)
+        {
+            ArrayList listado = (ArrayList)gvPiezasGuardadas.ItemsSource;
+            Pieza piezaSeleccionada = (Pieza) gvPiezasGuardadas.SelectedItem;
+            for (int i = 0; i < listado.Count; i++)
+            {
+                Pieza piezaActual = (Pieza)listado[i];
+                if (piezaActual.codigo == piezaSeleccionada.codigo)
+                {
+                    listado.RemoveAt(i);
+                    break;
+                }
+            }
+            gvPiezasGuardadas.ItemsSource = listado;
+            gvPiezasGuardadas.Items.Refresh();
         }
 	}
 }
