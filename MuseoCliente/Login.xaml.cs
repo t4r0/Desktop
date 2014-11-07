13bd62443b5dae51a18644b46ddb8adacd77a0c8
Estuardo Díaz <esjoando555@gmail.com>
2014-11-07 11:01:58 -0600

    ACTUALIZACION
    Cambios en el login

--- MuseoCliente/Login.xaml.cs
+++ MuseoCliente/Login.xaml.cs
@@ -15,17 +15,29 @@ using MuseoCliente.Connection.Objects;
 using MuseoCliente.Properties;
 using MuseoCliente.Designer;
 using System.Threading.Tasks;
+using System.Threading;
 namespace MuseoCliente
 {
 	/// <summary>
 	/// Lógica de interacción para Login.xaml
 	/// </summary>
+    /// 
+
 	public partial class Login : Window
 	{
+        CancellationTokenSource cToken = new CancellationTokenSource();
+        CancellationToken token;
+        string s;
+        LoadingAnimation animation;
+
         Dictionary<string, string> dict = new Dictionary<string, string>();
 		public Login()
 		{
 			this.InitializeComponent();
+            animation = new LoadingAnimation{
+            HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch,
+            VerticalAlignment = System.Windows.VerticalAlignment.Stretch
+        };
 			
 			// A partir de este punto se requiere la inserción de código para la creación del objeto.
 		}
@@ -39,33 +51,40 @@ namespace MuseoCliente
         {
             StartApp();
         }
-        private void StartApp()
-        {
 
+        private async void StartApp()
+        {
             addAnimation();
-            Task<Usuario> t = new Task<Usuario>(() => login());
-            t.RunSynchronously();
-            ShowWindow(t.Result);
-
-        }
-        private void addAnimation()
-        {
-            LoadingAnimation animation = new LoadingAnimation
+            cToken = new CancellationTokenSource();
+            token = cToken.Token;
+            Task<Usuario> t = Task<Usuario>.Factory.StartNew(() => login(token));
+            await t;
+            if (!token.IsCancellationRequested)
+                ShowWindow(t.Result);
+            else
             {
-                HorizontalAlignment = System.Windows.HorizontalAlignment.Stretch,
-                VerticalAlignment = System.Windows.VerticalAlignment.Stretch,
-                Width = contentGrid.ActualWidth,
-                Height = contentGrid.ActualHeight,
-                Margin = new Thickness(0,0,0,0)
-            };
-            animation.message.Text = "Iniciando sesión";            
+                removeAnimation();
+            }
+        }
+
+        private void removeAnimation()
+        {
+            contentGrid.Children.Remove(animation);
+            message.Text = s;
+        }
+
+        private void addAnimation()
+        {           
+            animation.message.Text = "Cargando";    
+            animation.Width = contentGrid.ActualWidth;
+            animation.Height = contentGrid.ActualHeight;
             Grid.SetZIndex(animation, 999);
-            LayoutRoot.Children.Add(animation);
+            contentGrid.Children.Add(animation);
             dict["username"] = txtUsuario.Text;
             dict["password"] = txtPassword.Password;
         }
 
-        private Usuario login()
+        private Usuario login(CancellationToken token)
         {
             Usuario user = new Usuario();
             try
@@ -78,9 +97,10 @@ namespace MuseoCliente
             }
             catch (Exception ex)
             {
-                throw ex;
+                cToken.Cancel();
+                s = ex.Message;
             }
-          
+            return null;
         }
 
         private void ShowWindow(Usuario user)
@@ -91,6 +111,7 @@ namespace MuseoCliente
             main.ShowDialog();
             this.Close();
         }
+
         private void Window_Loaded(object sender, RoutedEventArgs e)
         {
             txtUsuario.Focus();
