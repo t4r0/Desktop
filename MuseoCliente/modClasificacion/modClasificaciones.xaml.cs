3174fb57c369ecaf246ef32694b3c2d8256dd9f9
JEscalante <JEscalanteGT@gmail.com>
2014-11-16 15:36:19 -0600

    Arreglo de busqueda, edición y nuevo del modulo de clasificaciones

--- MuseoCliente/modClasificacion/modClasificaciones.xaml.cs
+++ MuseoCliente/modClasificacion/modClasificaciones.xaml.cs
@@ -41,6 +41,7 @@ namespace MuseoCliente
             Task<ArrayList> t = Task<ArrayList>.Factory.StartNew(() => categ.regresarTodo());
             await t;
             gvCategorias.ItemsSource = t.Result;
+            gvCategorias.SelectedValuePath = "id";
         }
 
         private async void CargarColecciones()
@@ -48,6 +49,7 @@ namespace MuseoCliente
             Task<ArrayList> t = Task<ArrayList>.Factory.StartNew(() => colec.regresarTodo());
             await t;
             gvColecciones.ItemsSource  = t.Result;
+            gvColecciones.SelectedValuePath = "id";
         }
 
         private async void CargarClasificaciones()
@@ -55,6 +57,26 @@ namespace MuseoCliente
             Task<ArrayList> t = Task<ArrayList>.Factory.StartNew(() => clasif.regresarTodo());
             await t;
             gvClasificaciones.ItemsSource = t.Result;
+            gvClasificaciones.SelectedValuePath = "id";
+        }
+
+        private async void buscarColecciones(string nombre)
+        {
+            Task<ArrayList> task = Task<ArrayList>.Factory.StartNew(() => colec.consultarNombre(nombre));
+            await task;
+            gvColecciones.ItemsSource = task.Result;
+        }
+        private async void buscarCategorias(string nombre)
+        {
+            Task<ArrayList> task = Task<ArrayList>.Factory.StartNew(() => categ.consultarNombre(nombre));
+            await task;
+            gvCategorias.ItemsSource = task.Result;
+        }
+        private async void buscarClasificacion(string nombre)
+        {
+            Task<ArrayList> task = Task<ArrayList>.Factory.StartNew(() => clasif.consultarNombre(nombre));
+            await task;
+            gvClasificaciones.ItemsSource = task.Result;
         }
         private void btnNuevaCateg_Click(object sender, RoutedEventArgs e)
         {
@@ -86,6 +108,84 @@ namespace MuseoCliente
             modClasificacion frm = new modClasificacion();
             frm.borde = borde;
             frm.anterior = this;
+            borde.Child = frm;
+        }
+
+        private void txtBuscarColecciones_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtBuscarColecciones.Text.Length > 3)
+                buscarColecciones(txtBuscarColecciones.Text);
+            else
+                CargarColecciones();
+        }
+
+        private void btnBuscarColecciones_Click(object sender, RoutedEventArgs e)
+        {
+            buscarColecciones(txtBuscarColecciones.Text);
+        }
+
+        private void btnEditarColeccion_Click(object sender, RoutedEventArgs e)
+        {
+            modColeccion frm = new modColeccion();
+            frm.borde = borde;
+            frm.anterior = this;
+            if (gvColecciones.SelectedValue != null)
+            {
+                frm.modificar = true;
+                frm.id = Convert.ToInt16(gvColecciones.SelectedValue.ToString());
+            }
+            borde.Child = frm;
+        }
+
+        private void btnEditarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            modCategoria frm = new modCategoria();
+            frm.borde = borde;
+            frm.anterior = this;
+            if (gvCategorias.SelectedValue != null)
+            {
+                frm.modificar = true;
+                frm.id = Convert.ToInt16(gvCategorias.SelectedValue.ToString());
+            }
+            borde.Child = frm;
+        }
+
+        private void txtBuscarCategoria_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtBuscarCategoria.Text.Length > 3)
+                buscarCategorias(txtBuscarCategoria.Text);
+            else
+                CargarCategorias();
+        }
+
+        private void txtBuscarClasificacion_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            if (txtBuscarClasificacion.Text.Length > 3)
+                buscarClasificacion(txtBuscarClasificacion.Text);
+            else
+                CargarClasificaciones();
+        }
+
+        private void btnBuscarCategoria_Click(object sender, RoutedEventArgs e)
+        {
+            buscarCategorias(txtBuscarCategoria.Text);
+        }
+
+        private void btnBuscarClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            buscarClasificacion(txtBuscarClasificacion.Text);
+        }
+
+        private void btnEditarClasificacion_Click(object sender, RoutedEventArgs e)
+        {
+            modClasificacion frm = new modClasificacion();
+            frm.borde = borde;
+            frm.anterior = this;
+            if (gvClasificaciones.SelectedValue != null)
+            {
+                frm.modificar = true;
+                frm.id = Convert.ToInt16(gvClasificaciones.SelectedValue.ToString());
+            }
             borde.Child = frm;
         }
 	}
